67e0230a4514839f8f5d5c4e744b57b71b77be56
Anton <kondratkov.anton@yandex.ru>
2018-12-06 20:32:23 +0700

    regex

--- unit_1/src/main/java/edu/RandomText.java
+++ unit_1/src/main/java/edu/RandomText.java
@@ -4,11 +4,13 @@ import java.io.*;
 import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
+/**
+ * Метод считывает знаки препинания и буквы разного регистра из одного файла, вычисляет заглавные буквы
+ * и записывает их в другой файл.
+ */
 public class RandomText {
-    private static String[] randomChar = {"A", "B", "c", "D", "f", "E", ",", "!", "."," ", "%"};
-
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    private static String[] randomChar = {"A", "B", "c", "D", "f", "E", ",", "!", ".", " ", "%"};
+    public static void main(String[] args) {
         randomText();
         textToUpperCase();
         readFileRandomText();
@@ -16,63 +18,80 @@ public class RandomText {
     }
     /**
      * Метод рандомно записывает буквы и знаки препинания из массива randomChar в файл RandomText.
-     * @throws IOException
      */
-    public static void randomText() throws IOException {
-        BufferedWriter bw = new BufferedWriter(new FileWriter("C:\\course\\RandomText.txt"));
-        Random random = new Random();
-        for (int i = 0; i < randomChar.length; i++) {
-            bw.write(randomChar[random.nextInt(randomChar.length)]);
+    public static void randomText() {
+        BufferedWriter bw = null;
+        try {
+            bw = new BufferedWriter(new FileWriter("C:\\course\\RandomText.txt"));
+            Random random = new Random();
+            for (int i = 0; i < randomChar.length; i++) {
+                bw.write(randomChar[random.nextInt(randomChar.length)]);
+            }
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        bw.close();
     }
     /**
      * Метод вычисляет заглавные буквы в файле RandomText и записывает их в файл UpperCase.
-     * @throws IOException
-     * @throws ClassNotFoundException
      */
-    public static void textToUpperCase() throws IOException, ClassNotFoundException {
-        BufferedReader br = new BufferedReader(new FileReader("C:\\course\\RandomText.txt"));
-        BufferedWriter bw = new BufferedWriter(new FileWriter("C:\\course\\UpperCase.txt"));
-
-        for (int i = 0; i < 11; i++) {
+    public static void textToUpperCase()  {
+        BufferedReader br = null;
+        BufferedWriter bw = null;
+        try {
+            br = new BufferedReader(new FileReader("C:\\course\\RandomText.txt"));
+            bw = new BufferedWriter(new FileWriter("C:\\course\\UpperCase.txt"));
             String result = br.readLine();
-            if(chekUpperCase(result)) {
-                bw.write(result);
+            char[] chars = result.toCharArray();
+            for (int i = 0; i < result.length(); i++) {
+                if (chekUpperCase(String.valueOf(chars[i]))) {
+                    bw.write(chars[i]);
+                }
             }
-
+            br.close();
+            bw.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        br.close();
-        bw.close();
     }
     /**
      * Метод отображает содержимое файла RandomText.
-     * @throws IOException
-     * @throws ClassNotFoundException
      */
-    public static void readFileRandomText() throws IOException, ClassNotFoundException {
-        BufferedReader br = new BufferedReader(new FileReader("C:\\course\\RandomText.txt"));
-
-        for (int i = 0; i < randomChar.length; i++) {
+    public static void readFileRandomText() {
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader("C:\\course\\RandomText.txt"));
             System.out.print(br.readLine());
+            br.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        br.close();
+    }
+     /**
+     * Метод отображает содержимое файла UpperCase.
+     */
+    public static void readFileUpperCase() {
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader("C:\\course\\UpperCase.txt"));
+            System.out.println();
+            System.out.print(br.readLine());
+            br.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     /**
-     * Метод отображает содержимое файла UpperCase.
-     * @throws IOException
-     * @throws ClassNotFoundException
+     * Метод проверяет соответствие переданной в аргументы строки регулярному выражению.
+     * @param f Переданная для проверки строка.
+     * @return false or true.
      */
-    public static void readFileUpperCase() throws IOException, ClassNotFoundException {
-        BufferedReader br = new BufferedReader(new FileReader("C:\\course\\UpperCase.txt"));
-
-        System.out.println();
-
-        for (int i = 0; i < randomChar.length; i++) {
-            System.out.print(br.readLine());
-        }
-        br.close();
-    }
     public static boolean chekUpperCase(String f) {
         Pattern p = Pattern.compile("^[A-Z]$");
         Matcher m = p.matcher(f);
